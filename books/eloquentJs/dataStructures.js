9e4aeb1370095dcd77b493b19848d3ef945d0793
Donald Vallejo <donny.vallejo@gmail.com>
2018-12-18 00:34:19 -0800

    added more notes and working on ch4 eloquent

---
+++ books/eloquentJs/dataStructures.js
@@ -0,0 +1,14 @@
+//count from start to end 
+// display those numbers
+function range(start, end) {
+  counted = [];
+  for(var i = start; i < end.length; i++) {
+    console.log("im an asshole");
+    counted.push(i);
+    console.log("im an asshole");
+    console.log("test", counted);
+}
+console.log("test2", counted);
+return counted
+}
+range(2, 9);
