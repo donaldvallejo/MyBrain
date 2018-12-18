72401b8fa8c06c492536e2e789a05d406ba30dd1
Donald Vallejo <donny.vallejo@gmail.com>
2018-12-17 18:32:22 -0800

    added more stuff

---
+++ books/eloquentJs/computingCorrelation.js
@@ -0,0 +1,15 @@
+function hasEvent(event , entry) {
+  return entry.events.indexOf(event) != -1;
+}
+
+
+functiontableFor(event , journal) {
+    var table = [0, 0, 0, 0];
+    for (var i = 0; i < journal.length; i++) {
+        var entry = journal[i], index = 0;
+        if (hasEvent(event , entry)) index += 1;
+        if (entry.squirrel) index += 2; table[index] += 1;
+        return table; 
+    }
+}
+console.log(tableFor("pizza", JOURNAL));
