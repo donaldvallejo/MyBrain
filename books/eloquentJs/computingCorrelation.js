47fbd8d4451cd60508676d14503cd6cd2796482f
Donald Vallejo <donny.vallejo@gmail.com>
2018-12-17 19:52:00 -0800

    aded stuff

--- books/eloquentJs/computingCorrelation.js
+++ books/eloquentJs/computingCorrelation.js
@@ -1,15 +1,11 @@
-function hasEvent(event , entry) {
-  return entry.events.indexOf(event) != -1;
-}
-
-
-functiontableFor(event , journal) {
-    var table = [0, 0, 0, 0];
-    for (var i = 0; i < journal.length; i++) {
-        var entry = journal[i], index = 0;
-        if (hasEvent(event , entry)) index += 1;
-        if (entry.squirrel) index += 2; table[index] += 1;
-        return table; 
+function gatherCorrelations(journal) {
+    var phis = {};
+    for (var entry = 0; entry < journal.length; entry++) {
+    var events = journal[entry].events;
+    for (var i = 0; i < events.length; i++) {
+    var event = events[i]; if (!(event in phis))
+    phis[event] = phi(tableFor(event , journal)); }
     }
-}
-console.log(tableFor("pizza", JOURNAL));
+    return phis; }
+    var correlations = gatherCorrelations(JOURNAL); 
+    console.log(correlations.pizza);
